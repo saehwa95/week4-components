fbcc166fae044b0c8c7c4dee4c87129bf2aba0ec
Saehwa95 <tpghk1002@naver.com>
2022-12-07 10:37:46 +0900

    폴더 구조 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Create from "../components/Create";
-import Header from "../components/Header";
 import TodoList from "../components/TodoList";
 
 const Main = () => {
   return (
     <>
-      <Header />
       <Create />
       <TodoList />
     </>
